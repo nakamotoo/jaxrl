95acb4e7c8a863b6bbe284ba4f821e8a03408245
nakamotoo <mitsuhiko0820.acm@gmail.com>
2023-03-15 04:09:23 -0700

    fix script

--- examples/run_antmaze.sh
+++ examples/run_antmaze.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 export XLA_PYTHON_CLIENT_PREALLOCATE=False
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=7
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
 # export WANDB_DISABLED=True
 
-env=antmaze-medium-play-v2
-env=antmaze-medium-diverse-v2
-env=antmaze-large-play-v2
+# env=antmaze-medium-play-v2
+# env=antmaze-medium-diverse-v2
+# env=antmaze-large-play-v2
 env=antmaze-large-diverse-v2
 
 # 4 5 6
-# for seed in 1 2 3
-for seed in 4 5 6
+# for seed in 1 3 5
+for seed in 2 4 6
 do
 python train_finetuning.py \
 --env_name=$env \
@@ -23,6 +23,6 @@ python train_finetuning.py \
 --num_pretraining_steps=1000000 \
 --eval_interval=50000 \
 --eval_episodes=50 \
---logging.project=AWAC-antmaze-rerun \
+--logging.project=AWAC-antmaze-rerun-Mar15 \
 --config.beta=1.0
 done
